0e30a975c6f2aab4bf3bf5e5bea42d3d204b5d11
Quang <buiminhquang188@gmail.com>
2021-09-17 21:59:28 +0700

    structural directive

---
+++ src/app/directive/structural/structural.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-structural',
+  templateUrl: './structural.component.html',
+  styleUrls: ['./structural.component.scss'],
+})
+export class StructuralComponent implements OnInit {
+  title = 'Structural Directives';
+  isLoggedIn = false;
+  username = 'quangb';
+  age = 0;
+  rappers = ['Đen Vâu', 'Binz', 'Karik', 'Wowy'];
+  login() {
+    this.isLoggedIn = true;
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
